49049513fdce1bfe7c931b1c8859be1cfc3f02c2
Feosen <iva588@yandex.ru>
2024-09-06 23:49:57 +0300

    feat: remove unsupported version URL.

--- fias/importer/version.py
+++ fias/importer/version.py
@@ -8,18 +8,12 @@ import urllib.request
 from dataclasses import dataclass
 from typing import Any, Dict, Iterable, List, Protocol
 
-from django.core.exceptions import ImproperlyConfigured
-from requests.exceptions import HTTPError
-from zeep.exceptions import XMLSyntaxError
-
-from fias.config import PROXY
 from fias.importer.signals import post_fetch_version, pre_fetch_version
 from fias.models import Version
 
 logger = logging.getLogger(__name__)
 
-WSDL_SOURCE = "http://fias.nalog.ru/WebServices/Public/DownloadService.asmx?WSDL"
-JSON_SOURCE = "http://fias.nalog.ru/WebServices/Public/GetAllDownloadFileInfo"
+JSON_SOURCE = "https://fias.nalog.ru/WebServices/Public/GetAllDownloadFileInfo"
 
 
 class ObjFileInfo:
@@ -107,39 +101,6 @@ def json_client_factory() -> Client:
     return JsonClient(JsonParser())
 
 
-def wsdl_client_factory() -> Client:
-    try:
-        try:
-            from zeep import __version__ as zver
-            from zeep.client import Client as ZeepClient
-
-            z_major, z_minor, z_sub = list(map(int, zver.split(".")))
-
-            zeep_client = ZeepClient(wsdl=WSDL_SOURCE)  # type: ignore
-            if z_minor < 20:
-                return WsdlClient(WsdlParser(), zeep_client)
-            else:
-                return WsdlClient(JsonParser(), zeep_client)
-
-        except ImportError:
-            pass
-        try:
-            from suds.client import Client as SudsClient
-
-            return WsdlClient(WsdlParser(), SudsClient(url=WSDL_SOURCE, proxy=PROXY or None))
-
-        except ImportError:
-            raise ImproperlyConfigured(
-                "Не найдено подходящей библиотеки для работы с WSDL." " Пожалуйста установите zeep или suds!"
-            )
-    except HTTPError as e:
-        print("Сайт не отвечает при запросе WSDL")
-        raise e
-    except Exception as e:
-        print(e)
-        raise e
-
-
 def update_or_create_version(item: VersionInfo, update_all: bool) -> None:
     ver, created = Version.objects.get_or_create(
         ver=item.version_id,
@@ -154,11 +115,7 @@ def update_or_create_version(item: VersionInfo, update_all: bool) -> None:
 def fetch_version_info(update_all: bool = False) -> None:
     logger.info("Version info updating.")
     pre_fetch_version.send(object.__class__)
-    try:
-        for item in wsdl_client_factory().version_info():
-            update_or_create_version(item, update_all)
-    except XMLSyntaxError:
-        for item in json_client_factory().version_info():
-            update_or_create_version(item, update_all)
+    for item in json_client_factory().version_info():
+        update_or_create_version(item, update_all)
     post_fetch_version.send(object.__class__)
     logger.info("Version info updated.")
